3486e1fabd6680d2de276dddc5800e406dd89080
Cantouchdis <benbokshi@gmail.com>
2018-06-14 07:33:30 +0200

    Empty Project

---
+++ app/src/main/java/com/example/ben/weatherapp/WeatherFragment.java
@@ -0,0 +1,10 @@
+package com.example.ben.weatherapp;
+
+import android.support.v4.app.Fragment;
+
+public class WeatherFragment extends Fragment {
+
+    public static WeatherFragment newInstance(){
+        return new WeatherFragment();
+    }
+}
